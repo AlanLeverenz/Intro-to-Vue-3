bb87551636bf5345e3a28e10b91772949d11ca56
Alan Leverenz <awleverenz@aol.com>
2021-02-17 10:03:52 -0500

    L3 coding challenge

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://www.brainyquote.com/quotes/walt_whitman_106900'
         }
     }
 })
